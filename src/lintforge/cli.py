96d5e9349d51e40181e783b8b062b3d2fc32cab5
vamsi krishna <kvamsi.nellore@gmail.com>
2026-09-03 20:15:43 +0530

    update cli function to accept codebase instead of single file

--- src/lintforge/cli.py
+++ src/lintforge/cli.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from typing import Annotated
 import typer
 import subprocess
 
@@ -7,11 +9,22 @@ app = typer.Typer(help="LintForge - AI code refactoring validation tool")
 def main():
     """LintForge - AI code refactoring validation tool"""
     pass
-
+def get_folder_option(option:str, description:str):
+    return Annotated[
+        Path,
+        typer.Option(
+            option,
+            exists=True,
+            file_okay=False,
+            dir_okay=True,
+            readable=True,
+            help=description
+        )
+    ]
 @app.command()
 def check(
-    original: typer.FileText = typer.Option(..., "--original", help="Original file without any modifications"),
-    refracted: typer.FileText = typer.Option(..., "--refracted", help="Refracted code by AI")
+    original: get_folder_option("--original", "Original codebase without any modifications"),
+    refracted: get_folder_option("--refracted", "Refracted codebase by AI")
 ):
     if original is None or refracted is None:
         print("[bold red] Error : [/bold red] :smiley:")
@@ -19,12 +32,12 @@ def check(
 
     # Static Analysis
     ruff_analysis_res = subprocess.run(
-            ["ruff", "check", "--stdin-filename", "original.py", "-"],
-            input = original.read(),
+            ["ruff", "check", "."],
+            cwd=str(original),
             capture_output = True,
             text=True
     )
     if ruff_analysis_res.returncode!=0 :
         print("Linting failed")
         raise typer.Exit(code=1)
-    
+    print('[bold magenta]Linting Success[/bold magenta]')
