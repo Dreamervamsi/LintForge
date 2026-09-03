7c3ba326662ddd3e6955389a81d432b838d70b20
vamsi krishna <kvamsi.nellore@gmail.com>
2026-09-03 20:41:17 +0530

    feat: initialise example refracted codebase

---
+++ examples/refracted/src/refracted/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] < target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return -1
+
+
+arr = [2, 5, 8, 12, 16, 23, 38, 45, 57]
+
+print(binary_search(arr, 23))
