f9ac723a687f576821870a09edd29f552d037034
vamsi krishna <kvamsi.nellore@gmail.com>
2026-09-04 13:08:11 +0530

    fix module not found issue by updating pyproject.toml

--- examples/refracted/tests/test_main.py
+++ examples/refracted/tests/test_main.py
@@ -2,7 +2,6 @@ from refracted import main
 
 
 def test_main_output(capsys):
-    """Test that main function prints expected output."""
     main()
     captured = capsys.readouterr()
     assert "Hello from refracted!" in captured.out
